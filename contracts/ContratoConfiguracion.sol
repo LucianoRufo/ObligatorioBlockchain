128742384706fc68124f8e2e70dfec151200a0c5
LucianoRufo <lucianorufo97@gmail.com>
2020-11-24 18:48:10 -0300

    Add subObjective function.

--- contracts/ContratoConfiguracion.sol
+++ contracts/ContratoConfiguracion.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.6.1;
 
 contract ContratoConfiguracion {
-   string public publicVariable;
-   
+
    struct Ahorrista {
         string ci;
         string name;
@@ -15,29 +14,125 @@ contract ContratoConfiguracion {
         bool isAuditor;
         uint debt;
         uint payed;
+        bool isApproved;
     }
 
-    uint public index = 0;
+    struct SubObjective {
+        string description;
+        uint payed;
+        uint totalToPay;
+        uint state; // {0, 1 , 2} = { en proceso, Aprobado, Ejecutado}
+    }
 
     address[] public ahorristas;
     address[] public gestores;
     address[] public auditores;
+    SubObjective[] public subObjectives;
 
     address public admin;
     address payable public savingAccount;
-    string public accountObjective;
+    string public accountObjectiveDescription;
+    uint public accountSavingsObjective;
+
     uint public actualSavings;
     uint public savingObjective;
     uint public cantAhorristas;
+    uint public maxAhorristas;
     uint public cantGestores;
-    uint public cantAuditores;
+    uint public maxGestores;
+    uint public maxAuditores;
     bool public accountEnabled;
+    uint public minimumContribution;
+    uint public minimumDeposit;
+    bool public isSavingVisible;
 
     mapping(address => Ahorrista) public ahorristaStructs; 
 
 
-    constructor() public {
-        publicVariable = 'Constructor in father';
+    constructor(address payable _savingAccount, string memory _objective,uint _savingsObjective, uint  _minimumDeposit ,uint  _minimumContribution, bool  _isSavingVisible ) public payable {
+        admin = msg.sender; //TODO: Admin no puede ser ni gestor ni auditor, agregar aquí a ahorristas.
+        savingAccount = _savingAccount;
+        accountObjectiveDescription = _objective;
+        accountSavingsObjective = _savingsObjective;
+        minimumContribution = _minimumContribution;
+        minimumDeposit = _minimumDeposit;
+        isSavingVisible = _isSavingVisible;
+        
     }
-    function PublicFunction() public virtual { publicVariable = 'PublicVariable in Config'; }
+
+
+    modifier isSaverActive(address saverAddress) {
+        require(ahorristaStructs[saverAddress].payed >= minimumContribution && ahorristaStructs[saverAddress].isApproved, "Only enabled account can receive deposits");
+        _;
+    }
+
+    modifier isContractEnabled() {
+        require(accountEnabled == true, "Only enabled account can receive deposits");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only the admin can call this function.");
+        _;
+    }
+
+    function addAhorristaAdmin(string memory _ci ,string memory _name,string memory _lastname, address _address,address _beneficiaryAddress,  bool _isGestor, bool _isAuditor) public onlyAdmin {
+    
+        ahorristas.push(_address);
+
+        ahorristaStructs[_address] = Ahorrista(
+            {
+                ci: _ci,
+                name: _name,
+                lastName: _lastname,
+                addedDate: now,
+                ahorristaAddress: _address,
+                beneficiaryAddress: _beneficiaryAddress,
+                isGestor: _isGestor,
+                isAuditor: _isAuditor,
+                debt: 0, //TODO: Add variable minimum
+                payed: 0,
+                isApproved: true
+            }
+        );
+         
+    }
+
+    function addAhorristaByDeposit(string memory _ci ,string memory _name,string memory _lastname, address _address,address _beneficiaryAddress,  bool _isGestor, bool _isAuditor) public  {
+        //TODO: Código para hacer la transferencia y depósito
+        ahorristas.push(_address);
+
+        ahorristaStructs[_address] = Ahorrista(
+            {
+                ci: _ci,
+                name: _name,
+                lastName: _lastname,
+                addedDate: now,
+                ahorristaAddress: _address,
+                beneficiaryAddress: _beneficiaryAddress,
+                isGestor: _isGestor,
+                isAuditor: _isAuditor,
+                debt: 0, //TODO: Add variable minimum
+                payed: 0,
+                isApproved: false
+            }
+        );
+         
+    }
+
+
+    function addSubObjective(string memory _description ,uint  _total) public onlyAdmin {
+    
+        subObjectives.push(SubObjective(
+            {
+                description: _description,
+                payed: 0,
+                totalToPay: _total,
+                state: 0
+            }
+        ));
+    }
+
+
+
 }
