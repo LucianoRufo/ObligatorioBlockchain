ad34d2ecc1a1ce147f4dd25dee2b7b0f32c377d9
LucianoRufo <lucianorufo97@gmail.com>
2020-11-24 22:20:54 -0300

    Fixed multiple bugs and modularized SubObjective voting handling in another contract.

--- contracts/ContratoConfiguracion.sol
+++ contracts/ContratoConfiguracion.sol
@@ -16,19 +16,18 @@ contract ContratoConfiguracion {
         uint payed;
         bool isApproved;
     }
-
-    struct SubObjective {
-        string description;
-        uint payed;
-        uint totalToPay;
-        uint state; // {0, 1 , 2} = { en proceso, Aprobado, Ejecutado}
-    }
+   
+    //Indexed event to execute and search by indexes. TODO: Vincular mejor el objetivo .
+    event Execute(
+        address indexed _gestorAddress,
+        address indexed _objectiveAddress,
+        uint _value
+    );
 
     address[] public ahorristas;
     address[] public gestores;
     address[] public auditores;
-    SubObjective[] public subObjectives;
-
+ 
     address public admin;
     address payable public savingAccount;
     string public accountObjectiveDescription;
@@ -41,12 +40,16 @@ contract ContratoConfiguracion {
     uint public cantGestores;
     uint public maxGestores;
     uint public maxAuditores;
+    uint public subObjectiveCounter;
     bool public accountEnabled;
     uint public minimumContribution;
     uint public minimumDeposit;
     bool public isSavingVisible;
+    bool public isVotingPeriod;
 
     mapping(address => Ahorrista) public ahorristaStructs; 
+    mapping(address => bool) public votedPerPeriodStruct; 
+    mapping(address => bool) public closingVotesPerPeriodStruct; 
 
 
     constructor(address payable _savingAccount, string memory _objective,uint _savingsObjective, uint  _minimumDeposit ,uint  _minimumContribution, bool  _isSavingVisible ) public payable {
@@ -57,27 +60,51 @@ contract ContratoConfiguracion {
         minimumContribution = _minimumContribution;
         minimumDeposit = _minimumDeposit;
         isSavingVisible = _isSavingVisible;
-        
+        isVotingPeriod = false;
+        subObjectiveCounter = 0;
     }
-
-
-    modifier isSaverActive(address saverAddress) {
-        require(ahorristaStructs[saverAddress].payed >= minimumContribution && ahorristaStructs[saverAddress].isApproved, "Only enabled account can receive deposits");
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only the admin can call this function.");
         _;
     }
+    modifier onlyAudit() {
+        require(ahorristaStructs[msg.sender].isAuditor, "Only the auditors can call this function.");
+        _;
+    }
+
+    modifier onlyGestor() {
+        require(ahorristaStructs[msg.sender].isGestor, "Only the managers can call this function.");
+        _;
+    }
+
+    modifier onlyAhorristaSimple() {
+        require(!ahorristaStructs[msg.sender].isGestor && ahorristaStructs[msg.sender].isAuditor && msg.sender != admin, "Only the admin can call this function.");
+        _;
+    }
+
+    modifier isSaverActive() {
+        require(ahorristaStructs[msg.sender].payed >= minimumContribution && ahorristaStructs[msg.sender].isApproved, "Only enabled account can receive deposits");
+        _;
+    }
+    
 
     modifier isContractEnabled() {
         require(accountEnabled == true, "Only enabled account can receive deposits");
         _;
     }
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only the admin can call this function.");
+   
+    modifier onVotingPeriod() {
+        require(isVotingPeriod == true, "You can only vote in voting period.");
+        _;
+    }
+    modifier hasNotVoted() {
+        require(!votedPerPeriodStruct[msg.sender] , "Only enabled account can receive deposits");
         _;
     }
 
+ 
     function addAhorristaAdmin(string memory _ci ,string memory _name,string memory _lastname, address _address,address _beneficiaryAddress,  bool _isGestor, bool _isAuditor) public onlyAdmin {
-    
         ahorristas.push(_address);
 
         ahorristaStructs[_address] = Ahorrista(
@@ -120,19 +147,19 @@ contract ContratoConfiguracion {
          
     }
 
-
-    function addSubObjective(string memory _description ,uint  _total) public onlyAdmin {
-    
-        subObjectives.push(SubObjective(
-            {
-                description: _description,
-                payed: 0,
-                totalToPay: _total,
-                state: 0
-            }
-        ));
+    function startGestorPostulation() public onlyAdmin {
+        //TODO
+    }
+    function startGestorVoting() public onlyAdmin {
+        //TODO
+    }
+    function finishGestorVoting() public onlyAdmin {
+        //TODO
     }
 
+    function postulateFor(bool postulateGestor, bool postulateAuditor) public onlyAhorristaSimple isSaverActive  {
+        //TODO
+    }
 
-
+    
 }
--- contracts/CuentaAhorro.sol
+++ contracts/CuentaAhorro.sol
@@ -1,14 +1,12 @@
 //SPDX-License-Identifier:MIT;
 pragma solidity ^0.6.1;
 
-import "./ContratoConfiguracion.sol";
+import "./SubObjectiveContract.sol";
 
-contract CuentaAhorro is ContratoConfiguracion {
+contract CuentaAhorro is SubObjectiveContract   {
     constructor(address payable _savingAccount, string memory _objective,uint _savingsObjective, uint  _minimumDeposit ,uint  _minimumContribution, bool  _isSavingVisible )
-      ContratoConfiguracion(_savingAccount,_objective,_savingsObjective,_minimumDeposit,_minimumContribution,_isSavingVisible) public { }
-
+      SubObjectiveContract(_savingAccount,_objective,_savingsObjective,_minimumDeposit,_minimumContribution,_isSavingVisible) public { }
     
- 
     function getBalance() public view onlyAdmin returns(uint256){
         return address(this).balance;
     }
@@ -17,7 +15,5 @@ contract CuentaAhorro is ContratoConfiguracion {
         uint balance = address(this).balance;
         savingAccount.transfer(balance);
     }
-
-   
 
 }
